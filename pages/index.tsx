b8e6842de0899f754e275929237cbc7c75ccca14
Axel Reyes <andreereyes0@gmail.com>
2025-01-09 20:04:48 -0600

    feat: create main banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/common/Header";
 import OrdersComponent from "@components/order/OrdersList";
+import Banner from "@components/home/Banner";
 
 const IndexPage = () => {
   return (
     <>
       <Header />
+      <Banner/>
       <OrdersComponent />
     </>
   );
